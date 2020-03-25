8b75b73017043d1a381e765ff66a96d6d69106f4
antonvinicius <antoniovrdacosta@gmail.com>
2020-03-25 15:55:00 -0300

    Criando tela profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 
 function Router() {
     return (
@@ -10,6 +11,7 @@ function Router() {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile}></Route>
             </Switch>
         </BrowserRouter>
     );
